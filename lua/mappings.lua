7df992ee7f21d561f0ad214b2c4ccc72062ddc9d
Daniel Oliveira <danieloliver.dev@gmail.com>
2024-05-29 10:38:13 -0300

    added new key maps for vertial and horizontal split

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,14 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- Gneral key mapping
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Window vertical split mappings
+map("n", "<leader>sv", ":vsplit<CR>", { desc = "Open vertical split" })
+map("n", "<leader>sfv", ":vsplit ", { desc = "Open file in vertical split" })
+
+-- Window horizontal split mappings
+map("n", "<leader>sh", ":split<CR>", { desc = "Open horizontal split" })
+map("n", "<leader>sfh", ":split ", { desc = "Open file in horizontal split" })
